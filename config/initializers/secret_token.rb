994d0e34b2c69403be4c354f2952a2684a9d990f
ichy <ichylinux@gmail.com>
2013-11-30 23:01:35 +0900

    dad new backbone_js_sample

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneJsSample::Application.config.secret_token = '7776081b79e43143bf199decd9e2ba53313c0b713a9e45245a2748d42eceda51bbd668ecd71d5abac5d967eb93637df5c39d94ca1005d576e451957326eb60b4'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+BackboneJsSample::Application.routes.draw do
+  # root :to => 'welcome#index'
+end
